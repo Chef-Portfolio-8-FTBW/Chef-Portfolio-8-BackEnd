3a36bbd3e5918fbca6adaf19fac7c69ada63ac8e
David Isakson <ikeman32@yahoo.com>
2019-12-21 14:03:20 -0800

    server authentication

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require(`morgan`);
 
+//const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require("../auth/authRouter.js");
 //const usersRouter = require('../users/users-router.js');
 
@@ -15,7 +16,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/auth", authRouter);
-//server.use('/api/users', usersRouter);
+//server.use('/api/users', authenticate, usersRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up", dbenv: process.env.DB_ENV });
